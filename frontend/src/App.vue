2d7feb808fa202cd648c49707a03a11c3c7c3250
ivangeorgiev97 <georgiev1313@abv.bg>
2020-01-26 21:13:36 +0200

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container" id="app">
+    <div id="nav">
+      <router-link to="/">Memories</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
