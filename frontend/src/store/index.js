e0192f05b750f2669bec218bf5a5ed1ec5c7865d
ivangeorgiev97 <georgiev1313@abv.bg>
2020-01-27 20:23:28 +0200

    small fixes

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import memories from './memories/memories'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    memories
   }
 })
+
---
+++ frontend/src/store/memories/memories.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+
+import axios from 'axios'
+
+const baseApiUrl = 'http://localhost:8000/api'
+
+export default {
+    namespaced: true,
+    state: {
+        memories: [],
+        currentMemory: {}
+    },
+    mutations: {
+        SET_MEMORIES(state, payload) {
+            state.memories = payload
+        },
+        SET_CURRENT_MEMORY(state, payload) {
+            state.currentMemory = payload
+        },
+        REMOVE_MEMORY(state, payload) {
+            state.clients.splice(payload.id, 1)
+        }
+    },
+    actions: {
+        setMemories(context) {
+            axios.get(`${baseApiUrl}/memories/getAll`).then((resp) => {
+                context.commit('SET_MEMORIES', resp.data.data)
+            })
+        },
+        addMemory(context, payload) {
+            axios.post(`${baseApiUrl}/memories/create`, {
+                title: payload.title,
+                description: payload.description
+            })
+            .then((resp) => {
+                console.log(resp)
+            })
+        }
+    },
+    getters: {
+        getMemories(state) {
+            return state.memories
+        }
+    }
+}
