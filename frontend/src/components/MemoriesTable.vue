fab12d269a3595cd14c7d2570a744686a1459452
Alex Draev <a.draev@codecoda.com>
2020-02-07 01:52:23 +0200

    fixed fields

--- frontend/src/components/MemoriesTable.vue
+++ frontend/src/components/MemoriesTable.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-        <b-table striped hover :items="items" />
+    <b-table striped hover :items="items" />
     <DataTable
       :header-fields="headerFields"
       :data="data || []"
@@ -15,22 +15,21 @@
         class="btn btn-info"
         value="Edit"
         @click="dtEditClick(props);"
-      >
-       <input
+      />
+      <input
         slot="actions-delete"
         slot-scope="props"
         type="button"
         class="btn btn-danger"
         value="Danger"
         @click="dtDeleteClick(props);"
-      >
+      />
     </DataTable>
   </div>
 </template>
 
 <script>
 import { DataTable } from "v-datatable-light";
-
 
 const addZero = value => ("0" + value).slice(-2);
 
@@ -46,54 +45,49 @@ const formatDate = value => {
 
 const initialData = [
   {
-    firstName: "Lucca",
-    lastName: "Lin",
-    hometown: "Melbourne",
-    dob: "13/02/1975",
+    id: "1",
+    title: "Lucca",
+    description: "Lin",
     created: new Date().getTime(),
     updated: new Date().getTime()
   },
   {
-    firstName: "Zahid",
-    lastName: "Werner",
-    hometown: "Sydney",
-    dob: "18/09/1979",
+    id: "2",
+    title: "Zahid",
+    description: "Werner",
     created: new Date().getTime(),
     updated: new Date().getTime()
   },
   {
-    firstName: "Gabriel",
-    lastName: "Griffiths",
-    hometown: "Chicago",
-    dob: "25/11/1984",
+    id: "3",
+    title: "Gabriel",
+    description: "Griffiths",
     created: new Date().getTime(),
     updated: new Date().getTime()
   },
   {
-    firstName: "Talha",
-    lastName: "Tucker",
-    
-    dob: "27/01/1999",
+    id: "4",
+    title: "Talha",
+    description: "Tucker",
     created: new Date().getTime(),
     updated: new Date().getTime()
   },
   {
-    firstName: "Aariz",
-    lastName: "Piper",
-    hometown: "Auckland",
-    dob: "11/07/1964",
+    id: "5",
+    title: "Aariz",
+    description: "Piper",
     created: new Date().getTime(),
     updated: new Date().getTime()
   }
 ];
 
 export default {
-    props: {
-        items: {
-            type: Array,
-            required: true
-        }
-    },
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
   name: "app",
   components: {
     DataTable
@@ -102,20 +96,16 @@ export default {
     return {
       headerFields: [
         {
-          name: "firstName",
-          label: "First Name"
+          name: "id",
+          label: "Id"
         },
         {
-          name: "lastName",
-          label: "Last Name"
+          name: "title",
+          label: "Title"
         },
         {
-          name: "hometown",
-          label: "Hometown"
-        },
-        {
-          name: "dob",
-          label: "Data of Birght"
+          name: "description",
+          label: "Description"
         },
         {
           name: "created",
@@ -127,9 +117,8 @@ export default {
           label: "Updated",
           format: formatDate
         },
-        "__slot:actions",        
-        "__slot:actions"            
-
+        "__slot:actions",
+        "__slot:actions"
       ],
       data: initialData.slice(0, 10),
       datatableCss: {
@@ -146,12 +135,10 @@ export default {
   },
   methods: {
     dtEditClick: props => alert("Click props:" + JSON.stringify(props)),
-    dtDeleteClick: props => alert("Click props:" + JSON.stringify(props)),
-
+    dtDeleteClick: props => alert("Click props:" + JSON.stringify(props))
   }
 };
 </script>
 <style scoped>
-
 </style>>
 
